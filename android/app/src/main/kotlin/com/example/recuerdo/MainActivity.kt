70a8bb2ce7489b924517cf2781736d3938e6d2c6
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2022-10-05 08:27:30 -0600

    primeros pasos

---
+++ android/app/src/main/kotlin/com/example/recuerdo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recuerdo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+// Aquí es donde ocurre toda la magia,
+// esta cosa es el comienzo del árbol de widgets
+void main() {
+  // En este caso comienzo con MaterialApp pero es más común comenzar con
+  // MyApp()
+  runApp(MaterialApp(
+    // A su propiedad home le doy Scaffold para comenzar a armar una página de
+    // inicio bastante sencilla, scaffold es como una sección con appbar y botn
+    home: Scaffold(
+      // Aquí configuro la appbar con color y texto
+      appBar: AppBar(
+        title: const Text("LA APP PARA RECORDAR"),
+        centerTitle: true,
+        // Para los colores puedo colocar entre corchetes un número que
+        // responde a una sombra de ese mismo color
+        backgroundColor: Colors.cyan[300],
+      ),
+      // Puedo poner widgets dentro de widgets y en este caso estoy usando
+      // a center para envolver todo porque centra todo y solo quiero
+      // mostrar un elemento que sería el texto hola compañeros
+      body: Center(
+        // Al texto le puedo cambiar varias cosas como el tamaño, grosor y espacio
+        // también le puedo cambiar la tipografía que usa
+        child: Text(
+          "Hola compañeros",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            // Aquí llamamos a una fuente según el nombre que le hayamos puesto
+            // en el pubspec.yaml
+            fontFamily: "PressStart2P-Regular",
+          ),
+        ),
+      ),
+      // Como parte del scaffold puedo utilizar un botón flotante que está
+      // siempre presente en la pantalla
+      floatingActionButton: FloatingActionButton(
+        // Función que se ejecuta cuando el botón se aplasta
+        onPressed: () {
+          print("hola");
+        },
+        // El child del boton puede ser texto o un ícono
+        child: const Text("click"),
+        backgroundColor: Colors.cyan[300],
+      ),
+    ),
+  ));
+}
+
+// Para añadir fuentes nos vamos al pubspec.yaml archivo
+// Se descomenta unas lineas de código de fonts y se añade la
+// que pusimos en la carpeta fonts
+
+// Para crearlo de manera rápida utilizar el sniped
+// stless y darle a enter
+// En flutter todo es una class, todo lo que se usa, el center, Text, etc
+// son solo intancias de clases
+// Stateless = no puede cambiar nada visual
+// Stateful = puede cambiar data y visual
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  // Para que al guardar se presenten los cambios y no tener que
+  // presionar r manualmente a cada momento se utiliza la build
+  // Todo lo que hace es mandar un widget, en la myoría de casos
+  // un árbol de widgets que en conjunto forman una pantalla
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
